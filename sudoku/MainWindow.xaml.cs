d97492b1c5406dd6826bc10e212ce972b1886e43
Evan Cui <Evan Cui>
2016-12-10 17:02:48 +0800

    Initial of sudoku

---
+++ sudoku/MainWindow.xaml.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sudoku
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int MaxCount = 9;
+        private const double OutlineThickness = 5.0;
+        private const double LineThickness = 0.5;
+        private const double BoxLineThickness = 1.0;
+        private readonly TimeSpan PutDuration = TimeSpan.FromSeconds(0.5); 
+
+        private SudokuData data;
+        private TextBlock[,] texts;
+        private TextBlock cursor;
+        private Random r = new Random((int)DateTime.Now.Ticks);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PutNumber(int n, int x, int y, bool delay = false)
+        {
+            var text = this.texts[x, y];
+            text.Text = $"{n}";
+
+            var previewText = new FormattedText(text.Text, CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface(text.FontFamily, text.FontStyle, text.FontWeight, text.FontStretch), text.FontSize, text.Foreground);
+
+            text.SetValue(Canvas.LeftProperty, OutlineThickness + x * this.CellSize + (this.CellSize - previewText.Width) / 2);
+            text.SetValue(Canvas.TopProperty, OutlineThickness + y * this.CellSize + (this.CellSize - previewText.Height) / 2);
+            var scaleTransform = text.RenderTransform as ScaleTransform
+                ?? new ScaleTransform(1.0, 1.0, previewText.Width / 2, previewText.Height / 2);
+            text.RenderTransform = scaleTransform;
+
+            DoubleAnimation drop = new DoubleAnimation()
+            {
+                Duration = new Duration(PutDuration),
+                BeginTime = delay ? TimeSpan.FromSeconds(this.r.NextDouble()) : TimeSpan.Zero,
+                DecelerationRatio = 1.0,
+            };
+
+            var show = drop.Clone();
+
+            if (n > 0)
+            {
+                drop.From = 10.0;
+                drop.To = 1.0;
+
+                show.From = 0.0;
+                show.To = 1.0;
+            }
+            else
+            {
+                drop.From = 1.0;
+                drop.To = 10.0;
+
+                show.From = 1.0;
+                show.To = 0.0;
+            }
+
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, drop);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, drop);
+            text.BeginAnimation(UIElement.OpacityProperty, show);
+        }
+
+        private double CellSize { get; set; }
+
+        private void DrawLines()
+        {
+            // draw outer border
+            Brush lineBrush = Brushes.Black;
+
+            this.CellSize = (canvas.ActualHeight - 2 * OutlineThickness) / MaxCount;
+            for (int i = 1; i < MaxCount; i++)
+            {
+                double offset = this.CellSize * i + OutlineThickness;
+                Func<double, double, double, double, Line> MakeLine = (x1, y1, x2, y2) =>
+                {
+                    return new Line() { X1 = x1, Y1 = y1, X2 = x2, Y2 = y2, Stroke = Brushes.Gray, StrokeThickness = i % 3 == 0 ? BoxLineThickness : LineThickness, StrokeLineJoin = PenLineJoin.Bevel };
+                };
+
+                double start = OutlineThickness;
+
+                this.canvas.Children.Add(MakeLine(offset, start, offset, this.canvas.ActualHeight - start));
+                this.canvas.Children.Add(MakeLine(start, offset, this.canvas.ActualHeight - start, offset));
+            }
+
+            this.texts = new TextBlock[MaxCount, MaxCount];
+
+            var numberList = Enumerable.Range(0, MaxCount).Select(x => Enumerable.Range(0, MaxCount)
+                .Select(y => new { X = x, Y = y })).SelectMany(a => a)
+                .Select(co => this.texts[co.X, co.Y] = new TextBlock()
+                {
+                    FontSize = 25.0,
+                    TextAlignment = TextAlignment.Center,
+                })
+                .ToList();
+
+            numberList.ForEach(t => this.canvas.Children.Add(t));
+
+            this.cursor = new TextBlock() { FontSize = 30.0, TextAlignment = TextAlignment.Center };
+            this.canvas.Children.Add(this.cursor);
+            this.canvas.Cursor = Cursors.None;
+            this.Cursor = Cursors.None;
+            this.cursor.Text = $"{this.CurrentNumber}";
+            Canvas.SetTop(this.cursor, Mouse.GetPosition(this.canvas).Y);
+            Canvas.SetLeft(this.cursor, Mouse.GetPosition(this.canvas).X);
+        }
+
+        private void InitGame()
+        {
+            this.data = new SudokuData(Difficulty.Basic);
+
+            var numberList = Enumerable.Range(0, MaxCount).Select(x => Enumerable.Range(0, MaxCount)
+                .Select(y => new { X = x, Y = y })).SelectMany(a => a)
+                .Select(co => new { X = co.X, Y = co.Y, N = this.data.Table[co.X, co.Y] })
+                .ToList();
+
+            numberList.ForEach(cell => this.PutNumber(cell.N, cell.X, cell.Y, true));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // draw lines;
+            this.DrawLines();
+            this.InitGame();
+        }
+
+        public int CurrentNumber { get; private set; } = 1;
+
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            this.CurrentNumber = ((this.CurrentNumber - 1 + e.Delta / 100) % 9 + 9) % 9 + 1;
+            this.currentNumber.Text = $"Current {this.CurrentNumber}";
+            this.cursor.Text = $"{this.CurrentNumber}";
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var p = e.GetPosition(this.canvas);
+
+            if (e.ButtonState == MouseButtonState.Released) return;
+            int current = this.CurrentNumber;
+
+            if (p.X > OutlineThickness && p.X < this.canvas.ActualWidth - OutlineThickness
+                && p.Y > OutlineThickness && p.Y < this.canvas.ActualHeight - OutlineThickness)
+            {
+                int x = (int)(p.X / this.CellSize);
+                int y = (int)(p.Y / this.CellSize);
+
+                switch (e.ChangedButton)
+                {
+                    case MouseButton.Right:
+                        if (this.data.Put(0, x, y) == PutResult.Cleared)
+                        {
+                            this.PutNumber(0, x, y);
+                        }
+                        else
+                        {
+                            // show occupied;
+                        }
+                        break;
+
+                    case MouseButton.Left:
+                        var result = this.data.Put(current, x, y);
+                        switch (result)
+                        {
+                            case PutResult.Complete:
+                                this.PutNumber(current, x, y);
+                                // show complete;
+                                break;
+
+                            case PutResult.Occupied:
+                                // show occupied;
+                                break;
+
+                            case PutResult.OK:
+                                this.PutNumber(current, x, y);
+                                // show OK
+                                break;
+
+                            default:
+                                // error;
+                                break;
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Canvas.SetTop(this.cursor, e.GetPosition(this.canvas).Y);
+            Canvas.SetLeft(this.cursor, e.GetPosition(this.canvas).X);
+        }
+    }
+}
---
+++ sudoku/SudokuData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudoku
+{
+    internal enum Difficulty
+    {
+        Basic,
+        Medium,
+        Hard
+    }
+
+    internal enum PutResult
+    {
+        OK,
+        Occupied,
+        Cleared,
+        Complete,
+    }
+
+    internal class SudokuData
+    {
+        private const int MaxCount = 9;
+        public int[,] Table { get; private set; } = new int[MaxCount, MaxCount];
+        public bool[,] Readonly { get; private set; } = new bool[MaxCount, MaxCount];
+
+        public SudokuData(Difficulty d)
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            Enumerable.Range(0, MaxCount).Select(x => Enumerable.Range(0, MaxCount)
+                .Select(y => new { X = x, Y = y })).SelectMany(a => a)
+                .AsParallel().ForAll(co =>
+                {
+                    this.Table[co.X, co.Y] = r.Next(MaxCount) + 1;
+                });
+        }
+
+        private bool Check9(Func<int, int> fetchNumber)
+        {
+            var numbers = Enumerable.Range(0, MaxCount).Select(i => fetchNumber(i)).Distinct().OrderBy(i => i).ToArray();
+            return numbers.Length == MaxCount && numbers[0] != 0;
+        }
+
+        public PutResult CheckResult()
+        {
+            bool complete = true;
+            for (int i = 0; i < MaxCount; i++)
+            {
+                complete &= this.Check9(j => this.Table[i, j]);
+                if (!complete) return PutResult.OK;
+                complete &= this.Check9(j => this.Table[j, i]);
+                if (!complete) return PutResult.OK;
+
+                int row = (i / 3) * 3;
+                int column = (i % 3) * 3;
+                complete &= this.Check9(j => this.Table[column + j % 3, row + j / 3]);
+                if (!complete) return PutResult.OK;
+            }
+
+            return PutResult.Complete;
+        }
+
+        public PutResult Put(int n, int x, int y)
+        {
+            if (this.Readonly[x, y])
+            {
+                return PutResult.Occupied;
+            }
+
+            if (n == 0)
+            {
+                if (this.Table[x, y] != 0)
+                {
+                    this.Table[x, y] = n;
+                    return PutResult.Cleared;
+                }
+                else
+                {
+                    return PutResult.OK;
+                }
+            }
+            else
+            {
+                this.Table[x, y] = n;
+                return this.CheckResult();
+            }
+        }
+    }
+}
